435f9edf35d70cb57a548563b7f085783513b424
Iryna Maslianko <persocom01@gmail.com>
2022-04-05 00:10:02 +0300

    uncomment

--- src/main/java/com/hillel/lessons/lesson14/Main.java
+++ src/main/java/com/hillel/lessons/lesson14/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Driver lorryDriver = new Driver("18.12.1985", "Водитель Грузовика Вася");
         System.out.println(lorryDriver);
-        /*lorryDriver.setExperience(2);
+        lorryDriver.setExperience(2);
         Engine lorryEngine = new Engine(120, "LCompany");
         Lorry lorry = new Lorry("Lorry", lorryEngine, lorryDriver, "MAN");
         lorry.setCarrying(500);
@@ -17,7 +17,7 @@ public class Main {
         lorry.turnLeft();
         lorry.turnRight();
         lorry.stop();
-        System.out.println(lorry);*/
+        System.out.println(lorry);
 
     }
 }
